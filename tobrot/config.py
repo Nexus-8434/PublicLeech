40d96165b6479fb8ff40ff453ea80458e84406b5
Nexus-8434 <67866437+Nexus-8434@users.noreply.github.com>
2020-07-05 23:11:17 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,9 @@
+
+from tobrot.sample_config import Config
+
+
+class Config(Config):
+  TG_BOT_TOKEN = "1129227461:AAEOVG041LWdPlTtHB1pYVDQ9cMAh8GIcBw"
+     APP_ID = 1259167
+     API_HASH = "c2cfc1b3e78a1646117f0cccd4e2c22a"
+     AUTH_CHANNEL = [-1001467656980]
